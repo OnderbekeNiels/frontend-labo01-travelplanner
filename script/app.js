3c49d4287b46af461872932bb0a22a0f4ff4a84e
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-02-12 14:27:41 +0100

    api request + grid

--- script/app.js
+++ script/app.js
@@ -1,1 +1,27 @@
 console.log("script loaded");
+
+const continents = {
+    africa: "Africa",
+    omerica: "America",
+    asia: "Asia",
+    europe: "Europe",
+    oceania: "Oceania",
+  },
+  endpoint = "https://restcountries.eu/rest/v2";
+
+let countryHolder;
+
+const getCountries = async (continent) => {
+  const data = await get(`${endpoint}/region/${continent}`);
+  console.log({ data });
+  // !curly brackets zorgen dat als een object wordt weergegeven.
+};
+
+const getDomElements = () => {
+  countryHolder = document.querySelector(".js-countries");
+  getCountries(continents.europe);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  getDomElements();
+});

3f885c110a6156dd1924a8f56d0cc43479b53770
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-02-20 12:09:56 +0100

    settings slider move

--- script/app.js
+++ script/app.js
@@ -143,6 +143,24 @@ const getDomElements = () => {
   updateCounter(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
 };
 
+const listenToClickSettings = () =>{
+let isOpen = false;
+document.querySelector(".js-settings-button").addEventListener("click", () => {
+  if (isOpen) {
+    document
+      .querySelector(".c-settings")
+      .classList.remove("c-settings--accessible");
+    isOpen = false;
+  } else {
+    document
+      .querySelector(".c-settings")
+      .classList.add("c-settings--accessible");
+    isOpen = true;
+  }
+});
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   getDomElements();
+  listenToClickSettings();
 });
